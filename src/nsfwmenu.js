e730b42b7cac8ea89ae84791a83c624fb0f115b7
Burraku-Black <72658799+Blacxyoung@users.noreply.github.com>
2021-05-13 17:48:40 -0400

    Add files via upload

---
+++ src/nsfwmenu.js
@@ -0,0 +1,18 @@
+const nsfwmenu = (prefix, pushname) => {
+    return `Comandos para raritos
+    ◪ *NSFW*
+  │
+  ├─ ❏ ${prefix}nsfwbobs
+  ├─ ❏ ${prefix}nsfwsidebobs
+  ├─ ❏ ${prefix}nsfwahegao
+  └─ ❏ ${prefix}nsfwfeets
+
+  _El bot nesecita admin y tener activado los NSFW_\n _Digita_\n ${prefix}*nsfw 1*
+
+Algunas funciones fueron eliminas 
+
+by ブラック`
+
+}
+
+exports.nsfwmenu = nsfwmenu
---
+++ src/toinmenu.js
@@ -0,0 +1,12 @@
+const toinmenu = (prefix, pushname) => {
+    return `◪ *Comandos de Burakku*
+    │
+    ├─ ❏ ${prefix}setprefix
+    ├─ ❏ ${prefix}block
+    ├─ ❏ ${prefix}bc
+    ├─ ❏ ${prefix}bcgc
+    └─ ❏ ${prefix}clearall`
+
+}
+
+exports.toinmenu = toinmenu
